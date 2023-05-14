5e025cbe74d2b0190daa124fc4c9c392e081e4f0
KarineCostaS <karine.costadev@gmail.com>
2023-05-14 13:58:38 -0300

    Adicionando Estrategias da Loja

--- cypress/e2e/smartt-store.cy.js
+++ cypress/e2e/smartt-store.cy.js
@@ -2,13 +2,28 @@ describe('SmarttStore Login', () => {
 
   beforeEach(() => {
     cy.visit('https://app.smarttbot.com/public/login')
-    cy.get('#login-username').type(Cypress.env('login')) // digita o username
-    cy.get('#login-password').type(Cypress.env('senha')) // digita a senha
+    cy.get('#login-username').type(Cypress.env('testeSeguidor')) // digita o username
+    cy.get('#login-password').type(Cypress.env('senhaSeguidor')) // digita a senha
     cy.get('#login-button').click() // clica no botão "Acessar"
   })
-    
-  })
+  
 
-it ('Login', () => {
+    
+  
+
+it ('Acessa as Estrategias da Loja', () => {
+  
+
+  cy.wait(1000)
+  cy.get('svg[data-icon="cart-plus"]').click(); //clica na LOja
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    //retorna falso os erros que podem danificar a automação, tratamento de exceção
+    return false //
+  })
+  
+  
+
+})
+
 
 })
